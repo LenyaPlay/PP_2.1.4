1478d6fdac7111408fe77444d88e9ab0599f4f97
LenyaPlay <lenyaplay@mail.ru>
2023-12-06 21:47:28 +0300

    finish 1

---
+++ src/main/java/app/model/TimerBean.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimerBean extends Timer {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
